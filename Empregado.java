b7016caddd2cdd1eaa4bbd619e85d55281314fc6
AlineGeMica <geovanna.oliveira01@fatec.sp.gov.br>
2023-02-23 17:48:09 -0300

    Incluuindo arquivos faltantes do commit anterior.

--- Empregado.java
+++ Empregado.java
@@ -41,4 +41,11 @@ public class Empregado{
     public void setIdade (int idade) {
         this.idade = idade;
     }
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo (int tipo) {
+        this.tipo = tipo;
+    }
 }
