4d1fe771d302e61a52d7ea6074e72db09bdd95ab
AlineGeMica <geovanna.oliveira01@fatec.sp.gov.br>
2023-02-23 18:03:41 -0300

    Primeiro teste realizado com sucesso.

--- Empregado.java
+++ Empregado.java
@@ -48,4 +48,23 @@ public class Empregado{
     public void setTipo (int tipo) {
         this.tipo = tipo;
     }
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public double getBonus() {
+        return bonus;
+    }
+    public double getComissao() {
+        return comissao;
+    }
+    public double getSalario() {
+        return salario;
+    }
 }
